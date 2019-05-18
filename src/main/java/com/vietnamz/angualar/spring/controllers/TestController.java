a0cffebf1b736079618a2c688b313400e878dfc0
Dang Tam <delgemoon@Dangs-MacBook-Pro.local>
2019-05-18 13:28:36 +0700

    expose delete/create api

--- src/main/java/com/vietnamz/angualar/spring/controllers/TestController.java
+++ src/main/java/com/vietnamz/angualar/spring/controllers/TestController.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +23,26 @@ public class TestController {
 	@RequestMapping(value = "/employees", method = RequestMethod.GET, produces = "application/json")
 	public List<Employee> firstPage() {
 		return employees;
+	}
+	
+	@DeleteMapping(path = { "/employees/{id}" })
+	public Employee delete(@PathVariable("id") int id) {
+		Employee deletedEmp = null;
+		for (Employee emp : employees) {
+			if (emp.getEmpId().equals(id)) {
+				employees.remove(emp);
+				deletedEmp = emp;
+				break;
+			}
+		}
+		return deletedEmp;
+	}
+
+	@PostMapping
+	public Employee create(@RequestBody Employee user) {
+		employees.add(user);
+		System.out.println(employees);
+		return user;
 	}
 	
 	private static List<Employee> createList() {
